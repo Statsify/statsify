212ecc8172fac7616010a11b90c9e8bcb604e637
jacobk999 <jababakoshy@gmail.com>
2022-01-16 10:59:49 -0500

    fix(playercontroller): changes the api operation of PlayerController#getPlayer

--- apps/api/src/player/player.controller.ts
+++ apps/api/src/player/player.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { Player } from '@statsify/schemas';
 import { HypixelCache } from '../hypixel/cache.enum';
 import { PlayerService } from './player.service';
 
-@ApiTags('player')
 @Controller('/player')
 export class PlayerController {
   public constructor(private readonly playerService: PlayerService) {}
 
+  @ApiOperation({ summary: 'Get a Player', tags: ['player'] })
   @ApiOkResponse({ type: Player })
   @Get()
   public async getPlayer(@Query('player') tag: string) {
