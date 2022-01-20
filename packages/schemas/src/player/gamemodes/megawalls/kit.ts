02655f496cfd726cb4466a22229ceb7be7422df0
eatmyvenom <eat.my.venomm@gmail.com>
2022-01-19 22:34:04 -0800

    feat(megawalls): add kit stats
    Added kit stats for each megawalls kit, this includes ratios and other basic data.

---
+++ packages/schemas/src/player/gamemodes/megawalls/kit.ts
@@ -0,0 +1,65 @@
+import { ratio } from '@statsify/math';
+import { APIData } from '@statsify/util';
+import { Field } from '../../../decorators';
+
+export class MegaWallsKit {
+  @Field()
+  public wins: number;
+
+  @Field()
+  public losses: number;
+
+  @Field()
+  public wlr: number;
+
+  @Field()
+  public kills: number;
+
+  @Field()
+  public deaths: number;
+
+  @Field()
+  public kdr: number;
+
+  @Field()
+  public finalKills: number;
+
+  @Field()
+  public finalDeaths: number;
+
+  @Field()
+  public fkdr: number;
+
+  @Field({ leaderboard: false })
+  public assists: number;
+
+  @Field({ leaderboard: false })
+  public timePlayed: number;
+
+  @Field({ leaderboard: false })
+  public witherDamage: number;
+
+  @Field({ leaderboard: false })
+  public witherKills: number;
+
+  public constructor(data: APIData, kit: string) {
+    kit = kit ? `${kit}_` : kit;
+
+    this.wins = data[`${kit}wins`];
+    this.losses = data[`${kit}losses`];
+    this.wlr = ratio(this.wins, this.losses);
+
+    this.kills = data[`${kit}kills`];
+    this.deaths = data[`${kit}deaths`];
+    this.kdr = ratio(this.kills, this.deaths);
+
+    this.finalKills = data[`${kit}final_kills`];
+    this.finalDeaths = data[`${kit}final_deaths`];
+    this.fkdr = ratio(this.finalKills, this.finalDeaths);
+
+    this.assists = data[`${kit}assists`];
+    this.timePlayed = data[`${kit}time_played`];
+    this.witherDamage = data[`${kit}wither_damage`];
+    this.witherKills = data[`${kit}wither_kills`];
+  }
+}
