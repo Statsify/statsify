26fceda0b3e53d7dd58b7f72082c952ba3324536
videogameking <42344274+jacobk999@users.noreply.github.com>
2022-03-06 11:59:55 -0500

    test(discord): add tests for `CommandBuilder`

---
+++ packages/discord/tests/command.builder.spec.ts
@@ -0,0 +1,92 @@
+import { ApplicationCommandOptionType, ApplicationCommandType } from 'discord-api-types/v10';
+import { Command, CommandBuilder, SubCommand } from '../src';
+
+describe('CommandBuilder', () => {
+  it('should read basic metadata', () => {
+    @Command({ description: 'test' })
+    class TestCommand {}
+
+    expect(CommandBuilder.scan(new TestCommand())).toEqual({
+      name: 'test',
+      description: 'test',
+      type: ApplicationCommandType.ChatInput,
+    });
+  });
+
+  it('should read subcommands', () => {
+    @Command({ description: 'test' })
+    class TestCommand {
+      @SubCommand({ description: 'test' })
+      public subcommand() {
+        //
+      }
+    }
+
+    expect(CommandBuilder.scan(new TestCommand())).toEqual({
+      name: 'test',
+      description: 'test',
+      type: ApplicationCommandType.ChatInput,
+      options: [
+        {
+          name: 'subcommand',
+          description: 'test',
+          type: ApplicationCommandOptionType.Subcommand,
+        },
+      ],
+    });
+  });
+
+  it('should read groups', () => {
+    @Command({ description: 'test' })
+    class GroupCommand {}
+
+    @Command({ description: 'test', groups: [GroupCommand] })
+    class TestCommand {}
+
+    expect(CommandBuilder.scan(new TestCommand())).toEqual({
+      name: 'test',
+      description: 'test',
+      type: ApplicationCommandType.ChatInput,
+      options: [
+        {
+          name: 'group',
+          description: 'test',
+          type: ApplicationCommandOptionType.SubcommandGroup,
+        },
+      ],
+    });
+  });
+
+  it('should read groups with subcommands', () => {
+    @Command({ description: 'test' })
+    class GroupCommand {
+      @SubCommand({ description: 'test' })
+      public subcommand() {
+        //
+      }
+    }
+
+    @Command({ description: 'test', groups: [GroupCommand] })
+    class TestCommand {}
+
+    expect(CommandBuilder.scan(new TestCommand())).toEqual({
+      name: 'test',
+      description: 'test',
+      type: ApplicationCommandType.ChatInput,
+      options: [
+        {
+          name: 'group',
+          description: 'test',
+          type: ApplicationCommandOptionType.SubcommandGroup,
+          options: [
+            {
+              name: 'subcommand',
+              description: 'test',
+              type: ApplicationCommandOptionType.Subcommand,
+            },
+          ],
+        },
+      ],
+    });
+  });
+});
--- packages/discord/tests/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('test', () => {
-  it('should test', () => {
-    expect(true).toBe(true);
-  });
-});
