9d69e43364af19ac41626a4332a460d308fba9ce
ugcodrr <codrrrr@gmail.com>
2022-03-06 13:21:15 -0700

    fix(warlords): fix Warlords losses

--- packages/schemas/src/player/gamemodes/warlords/index.ts
+++ packages/schemas/src/player/gamemodes/warlords/index.ts
@@ -1,4 +1,4 @@
-import { ratio } from '@statsify/math';
+import { add, ratio, sub } from '@statsify/math';
 import { APIData } from '@statsify/util';
 import { Field } from '../../../decorators';
 import { WarlordsClass } from './class';
@@ -21,6 +21,9 @@ export class Warlords {
 
   @Field()
   public coins: number;
+
+  @Field()
+  public gamesPlayed: number;
 
   @Field()
   public wins: number;
@@ -51,8 +54,33 @@ export class Warlords {
 
     this.class = data.chosen_class || 'warrior';
     this.coins = data.coins;
+
+    this.gamesPlayed = Math.ceil(
+      add(
+        data.aquamancer_plays,
+        data.avenger_plays,
+        data.berserker_plays,
+        data.crusader_plays,
+        data.cryomancer_plays,
+        data.defender_plays,
+        data.earthwarden_plays,
+        data.mage_plays,
+        data.paladin_plays,
+        data.protector_plays,
+        data.pyromancer_plays,
+        data.revenant_plays,
+        data.shaman_plays,
+        data.spiritguard_plays,
+        data.thunderlord_plays,
+        data.warrior_plays
+      ) / 2
+    );
     this.wins = data.wins;
-    this.losses = data.losses;
+
+    // Warlords Losses in API are bugged (~90% accurate)
+    // https://hypixel.net/threads/warlords-skillrating-website.1560046/post-12210896
+    this.losses = sub(this.gamesPlayed, this.wins);
+
     this.wlr = ratio(this.wins, this.losses);
     this.kills = data.kills;
     this.deaths = data.deaths;
