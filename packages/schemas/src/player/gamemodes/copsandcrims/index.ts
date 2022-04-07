e0cc4561c1e6b53e5b958c2e93f350b9805704d3
eatmyvenom <eat.my.venomm@gmail.com>
2022-04-07 14:09:27 -0400

    feat(cvc): rework data format for cvc (#95)

--- packages/schemas/src/player/gamemodes/copsandcrims/index.ts
+++ packages/schemas/src/player/gamemodes/copsandcrims/index.ts
@@ -1,63 +1,31 @@
+import { add } from '@statsify/math';
 import { APIData } from '@statsify/util';
 import { Field } from '../../../decorators';
+import { Deathmatch, Defusal, GunGame } from './mode';
 
 export class CopsAndCrims {
   @Field()
   public coins: number;
 
   @Field()
-  public roundWins: number;
-
-  @Field()
   public wins: number;
 
   @Field()
-  public kills: number;
+  public defusal: Defusal;
 
   @Field()
-  public copKills: number;
+  public deathmatch: Deathmatch;
 
   @Field()
-  public criminalKills: number;
-
-  @Field()
-  public killsDeathmatch: number;
-
-  @Field()
-  public copKillsDeathmatch: number;
-
-  @Field()
-  public criminalKillsDeathmatch: number;
-
-  @Field()
-  public headshots: number;
-
-  @Field()
-  public shotsFired: number;
-
-  @Field()
-  public grenadeKills: number;
-
-  @Field()
-  public bombsPlanted: number;
-
-  @Field()
-  public bombsDefused: number;
+  public gunGame: GunGame;
 
   public constructor(data: APIData) {
     this.coins = data.coins;
-    this.wins = data.game_wins;
-    this.roundWins = data.round_wins;
-    this.kills = data.kills;
-    this.copKills = data.cop_kills;
-    this.criminalKills = data.criminal_kills;
-    this.killsDeathmatch = data.kills_deathmatch;
-    this.copKillsDeathmatch = data.cop_kills_deathmatch;
-    this.criminalKillsDeathmatch = data.criminal_kills_deathmatch;
-    this.headshots = data.headshot_kills;
-    this.shotsFired = data.shots_fired;
-    this.grenadeKills = data.grenade_kills;
-    this.bombsPlanted = data.bombs_planted;
-    this.bombsDefused = data.bombs_defused;
+
+    this.defusal = new Defusal(data);
+    this.deathmatch = new Deathmatch(data);
+    this.gunGame = new GunGame(data);
+
+    this.wins = add(this.defusal.wins, this.deathmatch.wins, this.gunGame.wins);
   }
 }
---
+++ packages/schemas/src/player/gamemodes/copsandcrims/mode.ts
@@ -0,0 +1,98 @@
+import { ratio } from '@statsify/math';
+import { APIData } from '@statsify/util';
+import { Field } from '../../../decorators';
+
+export class Defusal {
+  @Field()
+  public wins: number;
+
+  @Field()
+  public roundWins: number;
+
+  @Field()
+  public kills: number;
+
+  @Field()
+  public deaths: number;
+
+  @Field()
+  public kdr: number;
+
+  @Field()
+  public headshotKills: number;
+
+  @Field()
+  public assists: number;
+
+  @Field()
+  public bombsPlanted: number;
+
+  @Field()
+  public bombsDefused: number;
+
+  public constructor(data: APIData) {
+    this.wins = data.game_wins;
+    this.roundWins = data.round_wins;
+    this.kills = data.kills;
+    this.deaths = data.deaths;
+    this.kdr = ratio(this.kills, this.deaths);
+    this.headshotKills = data.headshot_kills;
+    this.assists = data.assists;
+    this.bombsPlanted = data.bombs_planted;
+    this.bombsDefused = data.bombs_defused;
+  }
+}
+
+export class Deathmatch {
+  @Field()
+  public wins: number;
+
+  @Field()
+  public kills: number;
+
+  @Field()
+  public deaths: number;
+
+  @Field()
+  public kdr: number;
+
+  @Field()
+  public assists: number;
+
+  public constructor(data: APIData) {
+    this.wins = data.game_wins_deathmatch;
+    this.kills = data.kills_deathmatch;
+    this.deaths = data.deaths_deathmatch;
+    this.kdr = ratio(this.kills, this.deaths);
+    this.assists = data.assists_deathmatch;
+  }
+}
+
+export class GunGame {
+  @Field()
+  public wins: number;
+
+  @Field()
+  public kills: number;
+
+  @Field()
+  public deaths: number;
+
+  @Field()
+  public kdr: number;
+
+  @Field()
+  public assists: number;
+
+  @Field({ leaderboard: false })
+  public fastestWin: number;
+
+  public constructor(data: APIData) {
+    this.wins = data.game_wins_gungame;
+    this.kills = data.kills_gungame;
+    this.deaths = data.deaths_gungame;
+    this.kdr = ratio(this.kills, this.deaths);
+    this.assists = data.assists_gungame;
+    this.fastestWin = data.fastest_win_gungame;
+  }
+}
