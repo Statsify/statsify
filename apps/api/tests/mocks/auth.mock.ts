4750ebdb20c63d39b1d76437a0494fc9a0dbdeca
eatmyvenom <eat.my.venomm@gmail.com>
2022-04-17 04:40:48 +0000

    feat(api): add key data headers to api response
    Adds headers with data on the users api key

--- apps/api/tests/mocks/auth.mock.ts
+++ apps/api/tests/mocks/auth.mock.ts
@@ -2,5 +2,11 @@ import { AuthService } from '../../src/auth/auth.service';
 import { MockClass } from './mock.type';
 
 export const authService: MockClass<AuthService> = {
-  limited: jest.fn().mockResolvedValue(true),
+  limited: jest.fn().mockResolvedValue({
+    canActivate: true,
+    used: 0,
+    limit: 999,
+    resetTime: 999,
+  }),
+  createKey: jest.fn().mockResolvedValue('618a96fec8b0493fa89427891049550b'),
 };
