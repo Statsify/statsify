c0f61191c8786bc9c86931198509891f92ecbb59
eatmyvenom <eat.my.venomm@gmail.com>
2022-04-18 06:38:03 -0700

    chore: update development tools (#115)
    Dev environment, debugger, and changelog

---
+++ .devcontainer/install.sh
@@ -0,0 +1,5 @@
+echo 'export STATSIFY="/worspaces/statsify"' >> ~/.zshrc
+echo 'export PATH="$STATSIFY/scripts:$PATH"' >> ~/.zshrc
+echo 'alias c=clear' >> ~/.zshrc
+cargo install git-cliff
+yarn install
