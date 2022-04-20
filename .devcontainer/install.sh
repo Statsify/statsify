09a4af2f37972aeccc144f63a7a1ee1a54362aa7
eatmyvenom <eat.my.venomm@gmail.com>
2022-04-20 04:42:38 +0000

    chore(devcontainer): remove useless path

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,5 +1,3 @@
-echo 'export STATSIFY="/worspaces/statsify"' >> ~/.zshrc
-echo 'export PATH="$STATSIFY/scripts:$PATH"' >> ~/.zshrc
 echo 'alias c=clear' >> ~/.zshrc
 cargo install git-cliff
 yarn install
