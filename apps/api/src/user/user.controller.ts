1f22f571d7ae4ba48cbe2a0ba8e0c00e2a6b128e
eatmyvenom <eat.my.venomm@gmail.com>
2022-04-28 23:02:27 -0700

    fix(api): use http methods the way they are intended

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Query } from '@nestjs/common';
+import { Controller, Delete, Get, Put, Query } from '@nestjs/common';
 import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ErrorResponse, GetUserResponse } from '@statsify/api-client';
 import { Auth, AuthRole } from '../auth';
@@ -24,7 +24,7 @@ export class UserController {
     };
   }
 
-  @Get('/verify')
+  @Put()
   @ApiOkResponse({ type: GetUserResponse })
   @ApiOperation({ summary: 'Verify a user' })
   @ApiBadRequestResponse({ type: ErrorResponse })
@@ -38,7 +38,7 @@ export class UserController {
     };
   }
 
-  @Delete('/unverify')
+  @Delete()
   @ApiOperation({ summary: 'Unverify a user' })
   @ApiOkResponse({ type: GetUserResponse })
   @ApiBadRequestResponse({ type: ErrorResponse })
