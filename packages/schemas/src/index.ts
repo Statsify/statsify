5ad7881f1a4cb4130d28ddbf703df80d3233520d
jacobk999 <jababakoshy@gmail.com>
2022-05-10 21:51:40 -0400

    feat(scripts): add script to create rank emojis

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -7,6 +7,7 @@ export * from './metadata/deserialize';
 export * from './metadata/metadata.interface';
 export * from './metadata/serialize';
 export * from './player';
+export { rankMap } from './player/util';
 export * from './rankedskywars';
 export * from './recentgames';
 export * from './skin';
@@ -16,3 +17,4 @@ export * from './util/leaderboard-scanner';
 export * from './util/selector';
 export * from './verify-codes';
 export * from './watchdog';
+
