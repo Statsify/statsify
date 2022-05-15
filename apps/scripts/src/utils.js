66afccfff96a5990844c289b933e7ca3d4176c55
jacobk999 <jababakoshy@gmail.com>
2022-05-15 13:40:04 -0400

    feat(scripts): add build watch script and fix purge script
    remove create-env script

--- apps/scripts/src/utils.js
+++ apps/scripts/src/utils.js
@@ -1,5 +1,7 @@
 import chalk from 'chalk';
+import { readdir } from 'fs/promises';
 import inquirer from 'inquirer';
+import { join } from 'path';
 export const inquirerLogger = (title, message, newLine = true) => {
   console.log(
     `${newLine ? '\n' : ''}${chalk.magenta('!')} ${chalk.bold(title)} ${chalk.magenta(message)}`
@@ -17,3 +19,20 @@ export const inquirerConfirmation = async (message = 'Are you sure?', selected =
       },
     ])
   ).confirmation;
+
+export const ROOT = '../../';
+export const FILE_ENDINGS = ['.tsx', '.mts', '.cts', '.ts'];
+export const FILE_ENDING_REGEX = new RegExp(FILE_ENDINGS.join('|'));
+
+/**
+ *
+ * @param {string} path
+ * @param {string} ignoredWorkspaces
+ */
+export async function fetchWorkspaces(path, ignoredWorkspaces = []) {
+  const workspaces = await readdir(join(ROOT, path));
+
+  return workspaces
+    .filter((workspace) => !ignoredWorkspaces.includes(workspace))
+    .map((workspace) => join(ROOT, path, workspace));
+}
