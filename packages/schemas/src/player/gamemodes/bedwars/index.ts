868e8ae3e5dffa79937eccbaaff2e4e56b621705
ugcodrr <codrrrr@gmail.com>
2022-05-18 21:49:56 -0600

    feat(schema): add BedWars challenges

--- packages/schemas/src/player/gamemodes/bedwars/index.ts
+++ packages/schemas/src/player/gamemodes/bedwars/index.ts
@@ -3,7 +3,7 @@ import { APIData } from '@statsify/util';
 import { Color, ColorCode } from '../../../color';
 import { Field } from '../../../metadata';
 import { Progression } from '../../../progression';
-import { BedWarsMode, DreamsBedWars } from './mode';
+import { BedWarsMode, ChallengesBedWars, DreamsBedWars } from './mode';
 import { getExpReq, getFormattedLevel, getLevel } from './util';
 
 export class BedWars {
@@ -64,6 +64,9 @@ export class BedWars {
   @Field()
   public dreams: DreamsBedWars;
 
+  @Field()
+  public challenges: ChallengesBedWars;
+
   public constructor(data: APIData = {}) {
     this.coins = data.coins;
     this.exp = data.Experience || 0;
@@ -107,6 +110,8 @@ export class BedWars {
     this.core.winstreak = this.overall.winstreak;
 
     this.dreams = new DreamsBedWars(data);
+
+    this.challenges = new ChallengesBedWars(data);
   }
 }
 
--- packages/schemas/src/player/gamemodes/bedwars/mode.ts
+++ packages/schemas/src/player/gamemodes/bedwars/mode.ts
@@ -175,3 +175,136 @@ export class DreamsBedWars {
     this.voidless = new DreamsBedWarsMode(data, 'voidless');
   }
 }
+
+export class ChallengesBedWars {
+  @Field({ leaderboard: { enabled: false } })
+  public uniqueChallenges: number;
+
+  @Field()
+  public totalChallenges: number;
+
+  @Field()
+  public renegade: number;
+
+  @Field()
+  public warmonger: number;
+
+  @Field()
+  public selfish: number;
+
+  @Field()
+  public minimumWage: number;
+
+  @Field()
+  public assassin: number;
+
+  @Field()
+  public regularShopper: number;
+
+  @Field()
+  public invisibleShop: number;
+
+  @Field()
+  public collector: number;
+
+  @Field()
+  public woodworker: number;
+
+  @Field()
+  public bridgingForDummies: number;
+
+  @Field()
+  public toxicRain: number;
+
+  @Field()
+  public defuser: number;
+
+  @Field()
+  public miningFatigue: number;
+
+  @Field()
+  public ultimateUHC: number;
+
+  @Field()
+  public sleightOfHand: number;
+
+  @Field()
+  public weightedItems: number;
+
+  @Field()
+  public socialDistancing: number;
+
+  @Field()
+  public swordless: number;
+
+  @Field()
+  public marksman: number;
+
+  @Field()
+  public patriot: number;
+
+  @Field()
+  public stamina: number;
+
+  @Field()
+  public oldMan: number;
+
+  @Field()
+  public cappedResources: number;
+
+  @Field()
+  public redLightGreenLight: number;
+
+  @Field()
+  public slowReflexes: number;
+
+  @Field()
+  public pacifist: number;
+
+  @Field()
+  public masterAssassin: number;
+
+  @Field()
+  public standingTall: number;
+
+  @Field()
+  public protectThePresident: number;
+
+  @Field()
+  public cantTouchThis: number;
+  public constructor(data: APIData) {
+    this.uniqueChallenges = data.bw_unique_challenges_completed;
+    this.totalChallenges = data.total_challenges_completed;
+
+    this.renegade = data.bw_challenge_no_team_upgrades; // Renegade, #1
+    this.warmonger = data.bw_challenge_no_utilities; // Warmonger, #2
+    this.selfish = data.bw_challenge_selfish; // Selfish, #3
+    this.minimumWage = data.bw_challenge_slow_generator; // Minimum Wage, #4
+    this.assassin = data.bw_challenge_assassin; // Assassin, #5
+    this.regularShopper = data.bw_challenge_reset_armor; // Regular Shopper, #6
+    this.invisibleShop = data.bw_challenge_invisible_shop; // Invisible Shop, #7
+    this.collector = data.bw_challenge_collector; // Collector, #8
+    this.woodworker = data.bw_challenge_woodworker; // Woodworker, #9
+    this.bridgingForDummies = data.bw_challenge_sponge; // Bridging for Dummies, #10
+    this.toxicRain = data.bw_challenge_toxic_rain; // Toxic Rain, #11
+    this.defuser = data.bw_challenge_defuser; // Defuser, #12
+    this.miningFatigue = data.bw_challenge_mining_fatigue; // Mining Fatigue, #13
+    this.ultimateUHC = data.bw_challenge_no_healing; // Ultimate UHC, #14
+    this.sleightOfHand = data.bw_challenge_hotbar; // Sleight of Hand, #15
+    this.weightedItems = data.bw_challenge_weighted_items; // Weighted Items, #16
+    this.socialDistancing = data.bw_challenge_knockback_stick_only; // Social Distancing, #17
+    this.swordless = data.bw_challenge_no_swords; // Swordless, #18
+    this.marksman = data.bw_challenge_archer_only; // Marksman, #19
+    this.patriot = data.bw_challenge_patriot; // Patriot, #20
+    this.stamina = data.bw_challenge_stamina; // Stamina, #21
+    this.oldMan = data.bw_challenge_no_sprint; // Old man, #22
+    this.cappedResources = data.bw_challenge_capped_resources; // Capped resources, #23
+    this.redLightGreenLight = data.bw_challenge_stop_light; // Red Light, Green Light, #24
+    this.slowReflexes = data.bw_challenge_delayed_hitting; // Slow Reflexes, #25
+    this.pacifist = data.bw_challenge_no_hitting; // Pacifist, #26
+    this.masterAssassin = data.bw_challenge_master_assassin; // Master Assassin, #27
+    this.standingTall = data.bw_challenge_no_shift; // Standing Tall, #28
+    this.protectThePresident = data.bw_challenge_protect_the_president; // Protect the President, #29
+    this.cantTouchThis = data.bw_challenge_cant_touch_this; // Can't touch this, #30
+  }
+}
