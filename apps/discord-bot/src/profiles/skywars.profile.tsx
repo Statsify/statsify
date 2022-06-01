6d49e5b17aa6e90edeaaaddfa48784f674c1858d
jacobk999 <jababakoshy@gmail.com>
2022-05-31 20:52:47 -0400

    refactor(skywars): refactor skywars profile and remove useless skywars table component

--- apps/discord-bot/src/profiles/skywars.profile.tsx
+++ apps/discord-bot/src/profiles/skywars.profile.tsx
@@ -4,55 +4,13 @@ import {
   formatProgression,
   Header,
   HeaderBody,
-  If,
   SidebarItem,
   Table,
 } from '#components';
-import { LocalizeFunction } from '@statsify/discord';
 import { JSX } from '@statsify/rendering';
-import type { SkyWarsMode, SKYWARS_MODES } from '@statsify/schemas';
+import type { SKYWARS_MODES } from '@statsify/schemas';
 import { formatTime, prettify } from '@statsify/util';
 import { BaseProfileProps } from './base.profile';
-
-interface SkyWarsModeTableProps {
-  modeStats: SkyWarsMode;
-  mode: 'overall' | 'insane' | 'normal';
-  t: LocalizeFunction;
-  width?: JSX.Measurement;
-}
-
-const SkyWarsModeTable: JSX.FC<SkyWarsModeTableProps> = ({ width, modeStats, mode, t }) => {
-  const stats = modeStats[mode];
-  const isOverall = mode === 'overall';
-  const size = isOverall ? 'regular' : 'small';
-
-  return (
-    <Table.table width={width}>
-      <Table.tr>
-        <Table.td title={t('stats.wins')} value={t(stats.wins)} color="§a" size={size} />
-        <Table.td title={t('stats.losses')} value={t(stats.losses)} color="§c" size={size} />
-        <Table.td title={t('stats.wlr')} value={t(stats.wlr)} color="§6" size={size} />
-      </Table.tr>
-      <Table.tr>
-        <Table.td title={t('stats.kills')} value={t(stats.kills)} color="§a" size={size} />
-        <Table.td title={t('stats.deaths')} value={t(stats.deaths)} color="§c" size={size} />
-        <Table.td title={t('stats.kdr')} value={t(stats.kdr)} color="§6" size={size} />
-      </Table.tr>
-      <If condition={isOverall}>
-        <Table.tr>
-          <Table.td title={t('stats.assists')} value={t(stats.assists)} color="§a" size={size} />
-          <Table.td
-            title={t('stats.playtime')}
-            value={formatTime(stats.playtime)}
-            color="§c"
-            size={size}
-          />
-          <Table.td title={t('stats.kit')} value={prettify(stats.kit)} color="§6" size={size} />
-        </Table.tr>
-      </If>
-    </Table.table>
-  );
-};
 
 export interface SkyWarsProfileProps extends BaseProfileProps {
   mode: typeof SKYWARS_MODES[number];
@@ -69,7 +27,7 @@ export const SkyWarsProfile: JSX.FC<SkyWarsProfileProps> = ({
   t,
 }) => {
   const { skywars } = player.stats;
-  const modeStats = skywars[mode];
+  const stats = skywars[mode].overall;
 
   const sidebar: SidebarItem[] = [
     [t('stats.coins'), t(skywars.coins), '§6'],
@@ -94,7 +52,23 @@ export const SkyWarsProfile: JSX.FC<SkyWarsProfileProps> = ({
           )}`}
         />
       </Header>
-      <SkyWarsModeTable mode="overall" modeStats={modeStats} t={t} />
+      <Table.table>
+        <Table.tr>
+          <Table.td title={t('stats.wins')} value={t(stats.wins)} color="§a" />
+          <Table.td title={t('stats.losses')} value={t(stats.losses)} color="§c" />
+          <Table.td title={t('stats.wlr')} value={t(stats.wlr)} color="§6" />
+        </Table.tr>
+        <Table.tr>
+          <Table.td title={t('stats.kills')} value={t(stats.kills)} color="§a" />
+          <Table.td title={t('stats.deaths')} value={t(stats.deaths)} color="§c" />
+          <Table.td title={t('stats.kdr')} value={t(stats.kdr)} color="§6" />
+        </Table.tr>
+        <Table.tr>
+          <Table.td title={t('stats.assists')} value={t(stats.assists)} color="§a" />
+          <Table.td title={t('stats.playtime')} value={formatTime(stats.playtime)} color="§c" />
+          <Table.td title={t('stats.kit')} value={prettify(stats.kit)} color="§6" />
+        </Table.tr>
+      </Table.table>
       <Footer logo={logo} premium={premium} />
     </Container>
   );
