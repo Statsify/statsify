dbdf654a2564ec9787144766ffb9384de0779ac8
jacobk999 <jababakoshy@gmail.com>
2022-06-01 18:30:56 -0400

    fix(commands): move assists to be after kdr

--- apps/discord-bot/src/profiles/woolwars.profile.tsx
+++ apps/discord-bot/src/profiles/woolwars.profile.tsx
@@ -58,9 +58,9 @@ export const WoolWarsProfile: JSX.FC<WoolWarsProfileProps> = ({
         </Table.tr>
         <Table.tr>
           <Table.td title={t('stats.kills')} value={t(stats.kills)} color="§a" />
-          <Table.td title={t('stats.assists')} value={t(stats.assists)} color="§e" />
           <Table.td title={t('stats.deaths')} value={t(stats.deaths)} color="§c" />
           <Table.td title={t('stats.kdr')} value={t(stats.kdr)} color="§6" />
+          <Table.td title={t('stats.assists')} value={t(stats.assists)} color="§e" />
         </Table.tr>
       </Table.table>
       <Footer logo={logo} premium={premium} />
