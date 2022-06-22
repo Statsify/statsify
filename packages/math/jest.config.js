a07d7df1257b77f5855aa81010e1bb7207ae98de
jacobk999 <jababakoshy@gmail.com>
2022-06-21 21:42:41 -0400

    fix: math jest config

--- packages/math/jest.config.js
+++ packages/math/jest.config.js
@@ -6,12 +6,16 @@
  * https://github.com/Statsify/statsify/blob/main/LICENSE
  */
 
+const fs = require("fs");
+
+const config = JSON.parse(fs.readFileSync(`../../.swcrc`, "utf8"));
+
 /**
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
   testMatch: ["<rootDir>/tests/**/*.spec.ts"],
   transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest",
+    "^.+\\.(t|j)sx?$": ["@swc/jest", config],
   },
 };
