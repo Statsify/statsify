7f916300a8b0bad6b76ed0830f4a6c8ae411dd28
jacobk999 <jababakoshy@gmail.com>
2022-06-21 21:29:43 -0400

    ci(actions): make linter auto commit

--- apps/scripts/src/api-key.js
+++ apps/scripts/src/api-key.js
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) Statsify
- *
- * This source code is licensed under the GNU GPL v3 license found in the
- * LICENSE file in the root directory of this source tree.
- * https://github.com/Statsify/statsify/blob/main/LICENSE
- */
-
 // ! I (ugcodrr) am not liable for any damages or loss of data caused by this script.
 
 import Redis from "ioredis";
