404f3b3570036a39fb3ba80f37bb5276ab1311b9
Jacob <jababakoshy@gmail.com>
2022-07-04 11:16:57 -0400

    feat(site): add styled components

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,8 +1,20 @@
+/**
+ * Copyright (c) Statsify
+ *
+ * This source code is licensed under the GNU GPL v3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ * https://github.com/Statsify/statsify/blob/main/LICENSE
+ */
+
 module.exports = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
+  compiler: {
+    styledComponents: true,
+  },
+  swcMinify: true,
 };
--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -1,5 +1,27 @@
-const Index = () => {
-  return <div>Hello World</div>;
-};
+/**
+ * Copyright (c) Statsify
+ *
+ * This source code is licensed under the GNU GPL v3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ * https://github.com/Statsify/statsify/blob/main/LICENSE
+ */
+
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #f00;
+`;
+
+const Index = () => (
+  <div>
+    <Button
+      onClick={() => {
+        console.log("clicked");
+      }}
+    >
+      Hello World
+    </Button>
+  </div>
+);
 
 export default Index;
