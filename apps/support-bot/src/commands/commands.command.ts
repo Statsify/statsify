101a9274b726c1f32a245d493c91ea60e39a1b29
jacobk999 <jababakoshy@gmail.com>
2022-07-06 13:47:53 -0400

    feat(support-bot): add top commands command

---
+++ apps/support-bot/src/commands/commands.command.ts
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) Statsify
+ *
+ * This source code is licensed under the GNU GPL v3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ * https://github.com/Statsify/statsify/blob/main/LICENSE
+ */
+
+import {
+  ApiService,
+  Command,
+  CommandContext,
+  EmbedBuilder,
+  ErrorMessage,
+  PaginateService,
+} from "@statsify/discord";
+import { STATUS_COLORS } from "@statsify/logger";
+import { UserTier } from "@statsify/schemas";
+import { arrayGroup, prettify } from "@statsify/util";
+
+const COMMANDS_PER_PAGE = 25;
+
+@Command({ description: (t) => t("commands.commands"), tier: UserTier.CORE })
+export class CommandsCommand {
+  public constructor(
+    private readonly apiService: ApiService,
+    private readonly paginateService: PaginateService
+  ) {}
+
+  public async run(context: CommandContext) {
+    const commands = await this.apiService.getCommandUsage();
+
+    if (!commands) throw new ErrorMessage("errors.unknown");
+
+    const totalCommands = commands.commands;
+    delete commands.commands;
+
+    const commandList = Object.entries(commands).sort((a, b) => b[1] - a[1]);
+    const groups = arrayGroup(commandList, COMMANDS_PER_PAGE);
+
+    return this.paginateService.scrollingPagination(
+      context,
+      groups.map((group, index) => () => this.createTopPage(index, group, totalCommands))
+    );
+  }
+
+  private createTopPage(
+    page: number,
+    commands: [commandName: string, usage: number][],
+    totalCommands: number
+  ) {
+    const embed = new EmbedBuilder()
+      .title("Commands")
+      .footer((t) => `Total: ${t(totalCommands)}`)
+      .color(STATUS_COLORS.info)
+      .description((t) =>
+        commands
+          .map(([command, usage], index) => {
+            const position = page * COMMANDS_PER_PAGE + index + 1;
+            const commandName = prettify(command);
+            const percentage = Math.round((usage / totalCommands) * 100);
+
+            return `\`#${position}\`${commandName}: **${t(usage)}** \`(${t(
+              percentage
+            )}%)\``;
+          })
+          .join("\n")
+      );
+
+    return embed;
+  }
+}
