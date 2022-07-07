cee811a32614fd2984a724e4c63161543870b6a0
jacobk999 <jababakoshy@gmail.com>
2022-07-06 23:02:42 -0400

    fix background

--- apps/site/components/Hero/index.tsx
+++ apps/site/components/Hero/index.tsx
@@ -18,7 +18,8 @@ const StyledHero = styled.div`
   align-items: center;
   justify-content: center;
 
-  background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2));
+  background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url("/hero.png");
 
   width: 100%;
 
