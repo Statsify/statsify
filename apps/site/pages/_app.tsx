d425115dc6e55ec0e4dd5eb34cf0de3661f43f28
jacobk999 <jababakoshy@gmail.com>
2022-07-07 22:55:59 -0400

    fix favicon

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -73,8 +73,24 @@ export default function App({ Component, pageProps }: AppProps) {
 
         <meta content="#187ccd" name="theme-color" />
 
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
 
         <meta httpEquiv="content-language" content="en" />
         <meta charSet="utf8" />
