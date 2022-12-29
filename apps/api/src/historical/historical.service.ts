cb39e2fcd660d6f49973cf7cfe1817e01fd4ac69
ugcodrr <codrrrr@gmail.com>
2022-12-29 16:40:32 -0700

    feat(historical): add a sweeper to update skippeed historical resets

--- apps/api/src/historical/historical.service.ts
+++ apps/api/src/historical/historical.service.ts
@@ -17,7 +17,7 @@ import {
 } from "@statsify/api-client";
 import { Daily, LastDay, LastMonth, LastWeek, Monthly, Weekly } from "./models";
 import { DateTime } from "luxon";
-import { Flatten, flatten } from "@statsify/util";
+import { Flatten, config, flatten } from "@statsify/util";
 import { HistoricalLeaderboardService } from "./leaderboards/historical-leaderboard.service";
 import { Inject, Injectable, Logger, forwardRef } from "@nestjs/common";
 import { InjectModel } from "@m8a/nestjs-typegoose";
@@ -43,8 +43,8 @@ type RawHistoricalResponse = [newPlayer: Player, oldPlayer: Player, isNew?: bool
 @Injectable()
 export class HistoricalService {
   private readonly logger = new Logger("HistoricalService");
-  private readonly job: SimpleIntervalJob;
-
+  private readonly resetJob: SimpleIntervalJob;
+  private readonly sweepJob: SimpleIntervalJob;
   public constructor(
     @Inject(forwardRef(() => PlayerService))
     private readonly playerService: PlayerService,
@@ -57,9 +57,16 @@ export class HistoricalService {
     @InjectModel(LastWeek) private readonly lastWeekModel: PlayerModel,
     @InjectModel(LastMonth) private readonly lastMonthModel: PlayerModel
   ) {
-    const task = new AsyncTask("historicalReset", this.resetPlayers.bind(this));
-    this.job = new SimpleIntervalJob({ minutes: 1 }, task);
-    this.job.start();
+    const resetTask = new AsyncTask("historicalReset", this.resetPlayers.bind(this));
+    const sweepTask = new AsyncTask("historicalSweep", this.checkSweepPlayers.bind(this));
+
+    this.resetJob = new SimpleIntervalJob({ minutes: 1 }, resetTask);
+    this.resetJob.start();
+
+    this.sweepJob = new SimpleIntervalJob({ minutes: 1 }, sweepTask, {
+      preventOverrun: true,
+    });
+    this.sweepJob.start();
   }
 
   public async resetPlayers() {
@@ -78,9 +85,49 @@ export class HistoricalService {
 
       if (player) {
         player.resetMinute = minute;
-        await this.reset(player, type);
+        await this.resetPlayer(player, type);
       } else {
         this.logger.error(`Could not reset player with uuid ${uuid}`);
+      }
+    });
+  }
+
+  public async checkSweepPlayers() {
+    await this.getAndSweep(this.dailyModel, this.lastDayModel, HistoricalTimes.DAILY);
+    await this.getAndSweep(this.weeklyModel, this.lastWeekModel, HistoricalTimes.WEEKLY);
+    await this.getAndSweep(
+      this.monthlyModel,
+      this.lastMonthModel,
+      HistoricalTimes.MONTHLY
+    );
+  }
+
+  // Checks for players who were skipped in the reset process
+  public async getAndSweep(
+    model: PlayerModel,
+    lastModel: PlayerModel,
+    type: HistoricalType
+  ) {
+    const sweepPlayers = await model
+      .find({ nextReset: { $lt: Math.round(DateTime.now().toMillis() / 1000) } })
+      .select({ uuid: true, resetMinute: true })
+      .limit(config("environment") === "dev" ? 20 : 100)
+      .lean()
+      .exec();
+
+    sweepPlayers.forEach(async ({ uuid, resetMinute }) => {
+      const player = await this.playerService.get(uuid, HypixelCache.LIVE);
+
+      if (player) {
+        const flatPlayer = flatten(player);
+
+        const doc = serialize(Player, flatPlayer);
+
+        doc.resetMinute = resetMinute ?? this.getMinute();
+
+        await this.reset(model, lastModel, doc, type, player);
+      } else {
+        this.logger.error(`Could not sweep player with uuid ${uuid}`);
       }
     });
   }
@@ -91,7 +138,7 @@ export class HistoricalService {
 
     player.resetMinute = time ?? this.getMinute();
 
-    return this.reset(player, type);
+    return this.resetPlayer(player, type);
   }
 
   /**
@@ -100,7 +147,7 @@ export class HistoricalService {
    * @param resetType Whether to reset daily, weekly, or monthly
    * @returns The flattened player data
    */
-  public async reset(player: Player, resetType: HistoricalType) {
+  public async resetPlayer(player: Player, resetType: HistoricalType) {
     const isMonthly =
       resetType === HistoricalTimes.MONTHLY || resetType === HistoricalTimes.LAST_MONTH;
 
@@ -116,54 +163,67 @@ export class HistoricalService {
     doc.resetMinute = player.resetMinute ?? this.getMinute();
     flatPlayer.resetMinute = doc.resetMinute;
 
-    const reset = async (
-      model: PlayerModel,
-      lastModel: PlayerModel,
-      doc: Flatten<Player>,
-      time: HistoricalType
-    ) => {
-      //findOneAndReplace doesn't unflatten the document so findOne and replaceOne need to be used separately
-      const last = await model.findOne({ uuid: doc.uuid }).lean().exec();
-
-      delete doc._id;
-      doc.lastReset = Math.round(DateTime.now().toMillis() / 1000);
-      doc.nextReset = this.getNextResetTime(doc.resetMinute, time);
-
-      if (last) delete last._id;
-
-      await Promise.all([
-        model.replaceOne({ uuid: doc.uuid }, doc, { upsert: true }).lean().exec(),
-        lastModel
-          .replaceOne({ uuid: doc.uuid }, (last ?? doc) as Player, { upsert: true })
-          .lean()
-          .exec(),
-        this.historicalLeaderboardService.addHistoricalLeaderboards(
-          time,
-          Player,
-          player,
-          "uuid",
-          true
-        ),
-      ]);
-    };
-
     const resets = [
-      reset(this.dailyModel, this.lastDayModel, doc, HistoricalTimes.DAILY),
+      this.reset(this.dailyModel, this.lastDayModel, doc, HistoricalTimes.DAILY, player),
     ];
 
     if (isWeekly)
       resets.push(
-        reset(this.weeklyModel, this.lastWeekModel, doc, HistoricalTimes.WEEKLY)
+        this.reset(
+          this.weeklyModel,
+          this.lastWeekModel,
+          doc,
+          HistoricalTimes.WEEKLY,
+          player
+        )
       );
 
     if (isMonthly)
       resets.push(
-        reset(this.monthlyModel, this.lastMonthModel, doc, HistoricalTimes.MONTHLY)
+        this.reset(
+          this.monthlyModel,
+          this.lastMonthModel,
+          doc,
+          HistoricalTimes.MONTHLY,
+          player
+        )
       );
 
     await Promise.all(resets);
 
     return deserialize(Player, flatPlayer);
+  }
+
+  public async reset(
+    model: PlayerModel,
+    lastModel: PlayerModel,
+    doc: Flatten<Player>,
+    time: HistoricalType,
+    player: Player
+  ) {
+    //findOneAndReplace doesn't unflatten the document so findOne and replaceOne need to be used separately
+    const last = await model.findOne({ uuid: doc.uuid }).lean().exec();
+
+    delete doc._id;
+    doc.lastReset = Math.round(DateTime.now().toMillis() / 1000);
+    doc.nextReset = this.getNextResetTime(doc.resetMinute, time);
+
+    if (last) delete last._id;
+
+    await Promise.all([
+      model.replaceOne({ uuid: doc.uuid }, doc, { upsert: true }).lean().exec(),
+      lastModel
+        .replaceOne({ uuid: doc.uuid }, (last ?? doc) as Player, { upsert: true })
+        .lean()
+        .exec(),
+      this.historicalLeaderboardService.addHistoricalLeaderboards(
+        time,
+        Player,
+        player,
+        "uuid",
+        true
+      ),
+    ]);
   }
 
   public async get(tag: string, type: HistoricalType): Promise<Player | null> {
@@ -233,7 +293,7 @@ export class HistoricalService {
       oldPlayer = deserialize(Player, flatten(oldPlayer));
     } else {
       newPlayer.resetMinute = this.getMinute();
-      oldPlayer = await this.reset(newPlayer, type);
+      oldPlayer = await this.resetPlayer(newPlayer, type);
       isNew = true;
     }
 
@@ -264,7 +324,7 @@ export class HistoricalService {
       if (!livePlayer) throw new PlayerNotFoundException();
 
       livePlayer.resetMinute = this.getMinute();
-      newPlayer = await this.reset(livePlayer, HistoricalTimes.MONTHLY);
+      newPlayer = await this.resetPlayer(livePlayer, HistoricalTimes.MONTHLY);
       isNew = true;
     }
 
