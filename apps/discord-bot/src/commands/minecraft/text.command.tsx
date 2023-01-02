7cc7641b12a18af87d060821b52b6ea13049b28e
Jacob Koshy <42344274+jacobk999@users.noreply.github.com>
2023-01-01 21:21:24 -0500

    feat(text): add alignment options to text command (#477)

--- apps/discord-bot/src/commands/minecraft/text.command.tsx
+++ apps/discord-bot/src/commands/minecraft/text.command.tsx
@@ -7,6 +7,7 @@
  */
 
 import {
+  ChoiceArgument,
   Command,
   CommandContext,
   IMessage,
@@ -14,25 +15,37 @@ import {
   TextArgument,
 } from "@statsify/discord";
 import { Multiline } from "#components";
+import { StyleLocation, render } from "@statsify/rendering";
 import { convertColorCodes } from "#lib/convert-color-codes";
 import { getTheme } from "#themes";
-import { render } from "@statsify/rendering";
 
 @Command({
   description: (t) => t("commands.text"),
-  args: [new TextArgument("content"), new NumberArgument("size", 1, 9)],
+  args: [
+    new TextArgument("content"),
+    new NumberArgument("size", 1, 9),
+    new ChoiceArgument({
+      name: "alignment",
+      choices: ["left", "center", "right"] as StyleLocation[],
+      required: false,
+    }),
+  ],
 })
 export class TextCommand {
   public async run(context: CommandContext): Promise<IMessage> {
     const user = context.getUser();
+
     const content = context.option<string>("content").trim();
     const size = Math.min(context.option<number>("size", 2), 9);
+    const alignment = context.option<StyleLocation>("alignment", "left");
 
     const text = convertColorCodes(content).replaceAll("\\n", "\n");
 
     const canvas = render(
       <div direction="column">
-        <Multiline size={size}>{text}</Multiline>
+        <Multiline size={size} align={alignment}>
+          {text}
+        </Multiline>
       </div>,
       getTheme(user)
     );
