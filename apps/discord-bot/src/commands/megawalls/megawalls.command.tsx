a5ee48afe958fb715b384e7be2c16b7d358f2e8e
jacobk999 <jababakoshy@gmail.com>
2023-02-20 17:35:31 -0500

    fix(prefixes): fix issue with prefixes between two multiples of 10

--- apps/discord-bot/src/commands/megawalls/megawalls.command.tsx
+++ apps/discord-bot/src/commands/megawalls/megawalls.command.tsx
@@ -12,38 +12,13 @@ import {
   ProfileData,
 } from "../base.hypixel-command";
 import { Command } from "@statsify/discord";
-import {
-  GameMode,
-  MEGAWALLS_MODES,
-  MegaWallsKit,
-  MegaWallsModes,
-  Player,
-} from "@statsify/schemas";
+import { MEGAWALLS_MODES, MegaWallsModes } from "@statsify/schemas";
 import { MegaWallsProfile } from "./megawalls.profile";
 
 @Command({ description: (t) => t("commands.megawalls") })
 export class MegaWallsCommand extends BaseHypixelCommand<MegaWallsModes> {
   public constructor() {
     super(MEGAWALLS_MODES);
-  }
-
-  public filterModes(
-    player: Player,
-    modes: GameMode<MegaWallsModes>[]
-  ): GameMode<MegaWallsModes>[] {
-    const { megawalls } = player.stats;
-    const [overall, ...kits] = modes;
-
-    const filteredKits = kits
-      .slice(1, -1)
-      .sort(
-        (a, b) =>
-          (megawalls[b.api] as MegaWallsKit).points -
-          (megawalls[a.api] as MegaWallsKit).points
-      )
-      .slice(0, 24);
-
-    return [overall, ...filteredKits];
   }
 
   public getProfile(
