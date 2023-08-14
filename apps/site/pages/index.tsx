b8808e757c90347a27dbed16365134d28f3417a8
jacobk999 <jababakoshy@gmail.com>
2023-08-14 09:53:09 -0400

    update website

--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -35,7 +35,7 @@ const Index = () => (
     <StatCircleContainer>
       <StatCircle
         title="Servers"
-        value={90_000}
+        value={95_000}
         image={<Image src={serversImage} width={75} height={75} alt="Servers" />}
       />
       <StatCircle
