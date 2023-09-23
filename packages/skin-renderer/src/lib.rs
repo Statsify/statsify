bb16cc1c054b627e0996516520e0fd84d10980b6
Jacob Koshy <42344274+jacobk999@users.noreply.github.com>
2023-09-22 20:08:46 -0400

    feat(skins)!: add new skin renderer (#544)

---
+++ packages/skin-renderer/src/lib.rs
@@ -0,0 +1,45 @@
+mod buffer_dimensions;
+mod camera;
+mod error;
+mod instance;
+mod light;
+mod model;
+mod renderer;
+mod resources;
+mod skin_loader;
+mod texture;
+
+use async_once::AsyncOnce;
+use lazy_static::lazy_static;
+use napi::bindgen_prelude::*;
+use napi_derive::napi;
+use renderer::{SkinModelType, SkinRenderer};
+
+lazy_static! {
+  static ref SKIN_RENDERER: AsyncOnce<SkinRenderer> = AsyncOnce::new(async {
+    SkinRenderer::new(460, 800)
+      .await
+      .expect("Failed to initialize skin renderer")
+  });
+}
+
+#[napi]
+pub async fn render_skin(skin_url: Option<String>, is_slim: bool) -> Result<Buffer> {
+  let start = std::time::Instant::now();
+
+  let renderer = SKIN_RENDERER.get().await;
+
+  let model_type = if is_slim {
+    SkinModelType::Slim
+  } else {
+    SkinModelType::Classic
+  };
+
+  let skin_render = if let Some(skin_url) = skin_url {
+    renderer.render(model_type, &skin_url).await?
+  } else {
+    renderer.render_default_texture(model_type).await?
+  };
+
+  Ok(skin_render.into())
+}
