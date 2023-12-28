fc43533db4a0e32ea8c047d3da0c9a023e1cd21d
jacobk999 <jababakoshy@gmail.com>
2023-12-28 09:09:35 -0500

    fix tailwind config

--- apps/site/tailwind.config.ts
+++ apps/site/tailwind.config.ts
@@ -6,7 +6,7 @@
  * https://github.com/Statsify/statsify/blob/main/LICENSE
  */
 
-import type { Config } from "tailwindcss/colors";
+import type { Config } from "tailwindcss";
 
 export default {
 	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
