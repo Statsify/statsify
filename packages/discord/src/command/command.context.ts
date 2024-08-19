eaa5e6890806b6133b9233d311ab48ef928c1ec5
jacobk999 <jababakoshy@gmail.com>
2024-08-18 22:35:02 -0400

    add deprecated command messages

--- packages/discord/src/command/command.context.ts
+++ packages/discord/src/command/command.context.ts
@@ -7,13 +7,13 @@
  */
 
 import {
-  APIApplicationCommandInteractionDataBasicOption,
+  type APIApplicationCommandInteractionDataBasicOption,
   ApplicationCommandOptionType,
 } from "discord-api-types/v10";
-import { AbstractCommandListener } from "./abstract-command.listener.js";
-import { IMessage, LocalizeFunction, Message } from "#messages";
 import { User } from "@statsify/schemas";
 import { noop } from "@statsify/util";
+import type { AbstractCommandListener } from "./abstract-command.listener.js";
+import type { IMessage, LocalizeFunction, Message } from "#messages";
 import type { Interaction } from "#interaction";
 
 export class CommandContext {
