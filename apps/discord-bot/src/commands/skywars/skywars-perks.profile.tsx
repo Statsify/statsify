404e07cb34524e285edca2f2a93cf3f9a49d5a3c
jacobk999 <jababakoshy@gmail.com>
2025-02-12 15:33:13 -0500

    localize perk descriptions

--- apps/discord-bot/src/commands/skywars/skywars-perks.profile.tsx
+++ apps/discord-bot/src/commands/skywars/skywars-perks.profile.tsx
@@ -8,6 +8,7 @@
 
 import { Container, Footer, Header, WordWrap } from "#components";
 import { FormattedGame } from "@statsify/schemas";
+import { LocalizeFunction } from "@statsify/discord";
 import { prettify } from "@statsify/util";
 import type { BaseProfileProps } from "#commands/base.hypixel-command";
 import type { Image } from "skia-canvas";
@@ -49,43 +50,43 @@ export const SkyWarsPerksProfile = ({
         <div width="100%" height="1/6">
           <box height="100%"><text>1</text></box>
           <div width="remaining" height="100%">
-            <Perk perk={normalPerks.slot1} perkIcons={perkIcons} />
-            <Perk perk={insanePerks.slot1} perkIcons={perkIcons} />
+            <Perk perk={normalPerks.slot1} perkIcons={perkIcons} t={t} />
+            <Perk perk={insanePerks.slot1} perkIcons={perkIcons} t={t} />
           </div>
         </div>
         <div width="100%" height="1/6">
           <box height="100%"><text>2</text></box>
           <div width="remaining" height="100%">
-            <Perk perk={normalPerks.slot2} perkIcons={perkIcons} />
-            <Perk perk={insanePerks.slot2} perkIcons={perkIcons} />
+            <Perk perk={normalPerks.slot2} perkIcons={perkIcons} t={t} />
+            <Perk perk={insanePerks.slot2} perkIcons={perkIcons} t={t} />
           </div>
         </div>
         <div width="100%" height="1/6">
           <box height="100%"><text>3</text></box>
           <div width="remaining" height="100%">
-            <Perk perk={normalPerks.slot3} perkIcons={perkIcons} />
-            <Perk perk={insanePerks.slot3} perkIcons={perkIcons} />
+            <Perk perk={normalPerks.slot3} perkIcons={perkIcons} t={t} />
+            <Perk perk={insanePerks.slot3} perkIcons={perkIcons} t={t} />
           </div>
         </div>
         <div width="100%" height="1/6">
           <box height="100%"><text>4</text></box>
           <div width="remaining" height="100%">
-            <Perk perk={normalPerks.slot4} perkIcons={perkIcons} />
-            <Perk perk={insanePerks.slot4} perkIcons={perkIcons} />
+            <Perk perk={normalPerks.slot4} perkIcons={perkIcons} t={t} />
+            <Perk perk={insanePerks.slot4} perkIcons={perkIcons} t={t} />
           </div>
         </div>
         <div width="100%" height="1/6">
           <box height="100%"><text>5</text></box>
           <div width="remaining" height="100%">
-            <Perk perk={normalPerks.slot5} perkIcons={perkIcons} />
-            <Perk perk={insanePerks.slot5} perkIcons={perkIcons} />
+            <Perk perk={normalPerks.slot5} perkIcons={perkIcons} t={t} />
+            <Perk perk={insanePerks.slot5} perkIcons={perkIcons} t={t} />
           </div>
         </div>
         <div width="100%" height="1/6">
           <box height="100%"><text>6</text></box>
           <div width="remaining" height="100%">
-            <Perk perk={normalPerks.slot6} perkIcons={perkIcons} />
-            <Perk perk={insanePerks.slot6} perkIcons={perkIcons} />
+            <Perk perk={normalPerks.slot6} perkIcons={perkIcons} t={t} />
+            <Perk perk={insanePerks.slot6} perkIcons={perkIcons} t={t} />
           </div>
         </div>
       </div>
@@ -101,14 +102,18 @@ export const SkyWarsPerksProfile = ({
   );
 };
 
-function Perk({ perk, perkIcons }: { perk: string | undefined; perkIcons: Record<string, Image> }) {
+function Perk({ perk, perkIcons, t }: {
+  perk: string | undefined;
+  perkIcons: Record<string, Image>;
+  t: LocalizeFunction;
+}) {
   let name = "§cEmpty";
   let description = "No perks selected in this slot";
 
   if (perk) {
-    if (perk in perks) {
-      name = perks[perk].name;
-      description = perks[perk].description;
+    if (perk in PERK_NAMES) {
+      name = PERK_NAMES[perk];
+      description = t(`skywars-perks.${perk}`);
     } else {
       name = prettify(perk);
       description = "N/A";
@@ -132,7 +137,7 @@ function Perk({ perk, perkIcons }: { perk: string | undefined; perkIcons: Record
         />
         <div direction="column">
           <text align="left" margin={{ left: 6, top: 6, bottom: 2, right: 6 }}>§l{name}</text>
-          <WordWrap align="left" maxWidth={350} margin={{ left: 6, top: 2, bottom: 6, right: 6 }} color="§7">
+          <WordWrap align="left" maxWidth={350} margin={{ left: 6, top: 2, bottom: 6, right: 6 }} color="">
             §7{description}
           </WordWrap>
         </div>
@@ -141,34 +146,34 @@ function Perk({ perk, perkIcons }: { perk: string | undefined; perkIcons: Record
   );
 }
 
-const perks: Record<string, { name: string; description: string }> = {
-  bridger: { name: "§aBridger", description: "§a50% §7chance to not consume blocks" },
-  knowledge: { name: "§aKnowledge", description: "Gain §33 EXP §7Levels per kill" },
-  lucky_charm: { name: "§aLucky Charm", description: "§a30% §7chance to get a §6Golden Apple §7on kill" },
-  mining_expertise: { name: "§aMining Expertise", description: "Chance for 1 extra ore per mined block" },
-  nourishment: { name: "§aNourishment", description: "Refill hunger and saturation every kill" },
-  resistance_boost: { name: "§aResistance Boost", description: "§e15s §7of §bResistance II §7at game start" },
-  savior: { name: "§aSavior", description: "Gain §bAbsorption I §7for §e7s §7on enemy kill" },
-  annony_o_mite: { name: "§9Annony-o-mite", description: "§a10% §7chance to spawn §8Silverfish §7on bow hit" },
-  arrow_recovery: { name: "§9Arrow Recovery", description: "§a50% §7chance to recover arrow on hit" },
-  blazing_arrows: { name: "§9Blazing Arrows", description: "§a15% §7chance for fire arrows" },
-  environmental_expert: { name: "§9Environmental Expert", description: "§a50% §7reduced environmental damage" },
-  fat: { name: "§9Fat", description: "§e20s §7of §bAbsorption I §7at game start" },
-  speed_boost: { name: "§9Speed Boost", description: "Gain §bHaste II §7for §e300s §7at game start" },
-  barbarian: { name: "§6Barbarian", description: "Gain a §bSharpness §7level after 3 axe kills" },
-  black_magic: { name: "§6Black Magic", description: "§a30% §7chance for §5Ender Pearl §7after void kill" },
-  frost: { name: "§6Frost", description: "§a40% §7chance for §bSlowness I §7on bow hit" },
-  marksmanship: { name: "§6Marksmanship", description: "Gain §bPower I §7on bows after 2 bow kills" },
-  necromancer: { name: "§6Necromancer", description: "§a16% §7chance to spawn friendly §2Zombie §7on kill" },
-  revenge: { name: "§6Revenge", description: "Spawn a §4Spider §7on death" },
-  robbery: { name: "§6Robbery", description: "§a20% §7chance to drop item on fist hit" },
-  apothecary: { name: "§5Apothecary", description: "Extend positive potion effects by §a30%" },
-  diamond_in_the_rough: { name: "§5Diamond in the Rough", description: "§a5% §7chance for §bDiamond §7drop on kill" },
-  ender_end_game: { name: "§5Ender End Game", description: "§a+10% §7chance for §5Ender Pearl §7on chest refills" },
-  fruit_finder: { name: "§5Fruit Finder", description: "Guaranteed §6Golden Apple §7in first Middle Chest" },
-  librarian: { name: "§5Librarian", description: "Every 3 kills, get a random enchanted book" },
-  tenacity: { name: "§5Tenacity", description: "Heal 1 §cHeart §7per kill" },
-  fortune_teller: { name: "§5Fortune Teller", description: "Level 2 enchants guarantee §bProtection I §7and §bSharpness I" },
-  hide_and_seek: { name: "§5Hide and Seek", description: "Get a compass §e10s §7before first chest refill" },
-  diamondpiercer: { name: "§6Diamondpiercer", description: "§a20% §7chance for §a20% §7extra damage to §bDiamond Armor" },
+const PERK_NAMES: Record<string, string> = {
+  bridger: "§aBridger",
+  knowledge: "§aKnowledge",
+  lucky_charm: "§aLucky Charm",
+  mining_expertise: "§aMining Expertise",
+  nourishment: "§aNourishment",
+  resistance_boost: "§aResistance Boost",
+  savior: "§aSavior",
+  annony_o_mite: "§9Annony-o-mite",
+  arrow_recovery: "§9Arrow Recovery",
+  blazing_arrows: "§9Blazing Arrows",
+  environmental_expert: "§9Environmental Expert",
+  fat: "§9Fat",
+  speed_boost: "§9Speed Boost",
+  barbarian: "§6Barbarian",
+  black_magic: "§6Black Magic",
+  frost: "§6Frost",
+  marksmanship: "§6Marksmanship",
+  necromancer: "§6Necromancer",
+  revenge: "§6Revenge",
+  robbery: "§6Robbery",
+  apothecary: "§5Apothecary",
+  diamond_in_the_rough: "§5Diamond in the Rough",
+  ender_end_game: "§5Ender End Game",
+  fruit_finder: "§5Fruit Finder",
+  librarian: "§5Librarian",
+  tenacity: "§5Tenacity",
+  fortune_teller: "§5Fortune Teller",
+  hide_and_seek: "§5Hide and Seek",
+  diamondpiercer: "§6Diamondpiercer",
 };
