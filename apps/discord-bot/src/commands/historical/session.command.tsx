479605160448ca5d03788f61f5068e91251c7893
jacobk999 <jababakoshy@gmail.com>
2026-06-08 17:33:46 -0400

    fix: exclude bedwars challenges from session command

--- apps/discord-bot/src/commands/historical/session.command.tsx
+++ apps/discord-bot/src/commands/historical/session.command.tsx
@@ -10,14 +10,15 @@ import {
   ARCADE_MODES,
   ARENA_BRAWL_MODES,
   type ApiModeFromGameModes,
-  ApiSubModeForMode,
+  type ApiSubModeForMode,
   BEDWARS_MODES,
   BLITZSG_MODES,
   BUILD_BATTLE_MODES,
+  type BedWarsModes,
   COPS_AND_CRIMS_MODES,
   DUELS_MODES,
   GENERAL_MODES,
-  GameMode,
+  type GameMode,
   type GameModeWithSubModes,
   GameModes,
   MEGAWALLS_MODES,
@@ -124,7 +125,8 @@ export class SessionCommand {
     return this.run({
       context,
       modes: BEDWARS_MODES,
-      getProfile: (base, mode) => <BedWarsProfile {...base} mode={mode} />,
+      getProfile: (base, mode) => <BedWarsProfile {...base} mode={mode as Exclude<GameMode<BedWarsModes>, { api:"challenges" }>} />,
+      filterModes: (_, modes) => modes.filter((mode) => mode.api !== "challenges")
     });
   }
 
